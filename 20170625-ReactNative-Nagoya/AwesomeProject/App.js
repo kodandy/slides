b09a2bd7079c97fc96abcbfbf4573895dcde2591
Kazunari.NAKATA <o9o64456583@gmail.com>
2017-06-25 13:41:45 +0900

    Update

--- 20170625-ReactNative-Nagoya/AwesomeProject/App.js
+++ 20170625-ReactNative-Nagoya/AwesomeProject/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, WebView } from 'react-native';
+import { Constants } from 'expo';
 
 export default class App extends React.Component {
+  onNavigationStateChange = navState => {
+    if (navState.url.indexOf('https://www.google.com') === 0) {
+      const regex = /#access_token=(.+)/;
+      let accessToken = navState.url.match(regex)[1];
+      console.log(accessToken);
+    }
+  };
   render() {
+    const url = 'https://github.com/besutome/slides/tree/master/20170625-ReactNative-Nagoya/README.md';
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <WebView
+        source={{
+          uri: url,
+        }}
+        onNavigationStateChange={this.onNavigationStateChange}
+        startInLoadingState
+        scalesPageToFit
+        javaScriptEnabled
+        style={{ marginTop: 25 }}
+      />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
